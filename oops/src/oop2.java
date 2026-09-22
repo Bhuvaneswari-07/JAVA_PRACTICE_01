d51d7e27f3d1a7b1b00308b05dc1c97a26886ab3
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-09-22 20:57:52 +0530

    coonstructor example

---
+++ oops/src/oop2.java
@@ -0,0 +1,19 @@
+class Student{
+    String name;
+    int age;
+    Student(String n,int a) {
+        name = n;
+        age = a;
+        System.out.println("name:" + n);
+        System.out.println("age:" + a);
+
+
+    }
+}
+public class oop2 {
+    public static void main(String[] args){
+        Student s=new Student("bhuvana",18);
+
+
+    }
+}
